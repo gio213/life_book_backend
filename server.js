69ccdc245a16d528d48586475b0aa1b8334afbfc
Giorgi Patsia <gio.patsia@gmail.com>
2023-09-27 12:32:16 +0200

    added cors

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ import express from "express";
 import bodyParser from "body-parser";
 env.config({ path: "./.env" });
 import router from "./index.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  Credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use("/api", router);
 
