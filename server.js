bdd0865d5745847617c94864a50ad6e5a8907fd4
Giorgi Patsia <gio.patsia@gmail.com>
2023-09-28 12:41:45 +0200

    wip

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.use("/api", router);
 
 let port = process.env.PORT || 3000;
 console.log(port);
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
 });
