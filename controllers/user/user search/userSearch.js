50487a51537e7de01c76ccd9cf8219354a0dfaa5
Giorgi Patsia <gio.patsia@gmail.com>
2023-10-09 16:01:45 +0200

    fixed user search route

--- controllers/user/user search/userSearch.js
+++ controllers/user/user search/userSearch.js
@@ -1,18 +1,26 @@
 import pool from "../../../database/dbConnection.js";
 
 const userSearch = (req, res) => {
-  const { id } = req.decoded.user_id;
-  const userName = req.body.username;
-  const sql = `SELECT * FROM users WHERE username LIKE '%${userName}%'`;
-  pool.query(sql, (err, result) => {
+  const userId = req.decoded.user_id;
+
+  const userName = req.query.username;
+
+  if (!userName) {
+    return res.status(400).json("Username parameter is missing or empty.");
+  }
+
+  const sql = "SELECT * FROM users WHERE username LIKE ?";
+  const searchPattern = `%${userName}%`;
+
+  pool.query(sql, [searchPattern], (err, result) => {
     if (err) {
-      console.log(err);
-      res.status(500).json("Server error" + err);
+      console.error(err);
+      return res.status(500).json("Server error");
     }
     if (result.length === 0) {
-      res.status(400).json("No users found");
+      return res.status(404).json("No users found");
     } else {
-      res
+      return res
         .status(200)
         .json({ message: "Successfully retrieved users.", result });
     }
