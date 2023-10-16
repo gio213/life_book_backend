c4449773151b8e34ff6513d1898d86a7f91b66bd
Giorgi Patsia <gio.patsia@gmail.com>
2023-10-16 10:12:45 +0200

    fixed follow request endpoint

--- controllers/follows/follow_user_id.js
+++ controllers/follows/follow_user_id.js
@@ -1,24 +1,24 @@
 import pool from "../../database/dbConnection.js";
 
 const followUserId = async (req, res) => {
-  try {
-    const { id } = req.decoded.user_id;
-    const followee = req.body.followee;
+  const { id } = req.decoded.user_id;
+  const followee_id = req.body.followee_id;
+  const query =
+    await `INSERT INTO followers (follower_id, followee_id) VALUES (${id}, ${followee_id})`;
 
-    if (!followee) {
-      return res.status(400).json({ error: "Invalid 'followee' ID" });
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({
+        message: "You are already following this user",
+      });
+    } else {
+      console.log(result);
+      res.status(200).json({
+        message: "Successfully followed user",
+      });
     }
-
-    const sql = `INSERT INTO followers (follower_id, followee_id) VALUES (?, ?)`;
-    await pool.query(sql, [id, followee]);
-
-    res
-      .status(200)
-      .json(`User ${id} sent a follow request to user ${followee}`);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+  });
 };
 
 export default followUserId;
