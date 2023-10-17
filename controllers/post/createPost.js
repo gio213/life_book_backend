71846d9124fda27a7f989ec926a874cbb343f4d2
Giorgi Patsia <gio.patsia@gmail.com>
2023-10-17 15:35:05 +0200

    added file upload on creating post

--- controllers/post/createPost.js
+++ controllers/post/createPost.js
@@ -2,19 +2,25 @@ import pool from "../../database/dbConnection.js";
 
 const createPost = (req, res) => {
   const { id } = req.decoded.user_id;
-  console.log(id);
   const { content } = req.body;
-  const newPost = {
-    content,
-    user_id: id,
-  };
-  try {
-    const result = pool.query("INSERT INTO posts SET ?", newPost);
-    res.status(200).json({ message: "Post created successfully" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Server error" });
+  const picture = req.file;
+
+  let post_image = null;
+  if (picture) {
+    post_image = picture.path;
   }
+
+  pool.query(
+    "INSERT INTO posts (user_id, content, post_image) VALUES (?, ?, ?)",
+    [id, content, post_image],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+      res.status(200).json({ message: "Post created" });
+    }
+  );
 };
 
 export default createPost;
--- routes/post/createPost.js
+++ routes/post/createPost.js
@@ -1,6 +1,7 @@
 import createPost from "../../controllers/post/createPost.js";
 import passToken from "../../generate token /pass the token/passTheToke.js";
 import express from "express";
+import upload from "../../config/imageUpload.js";
 const router = express.Router();
-router.post("/create_post", passToken, createPost);
+router.post("/create_post", passToken, upload.single("post_image"), createPost);
 export default router;
