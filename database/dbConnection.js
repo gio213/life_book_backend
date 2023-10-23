5f1ba27bbf3b626f0ee39cc4c43650e4712c0b86
Giorgi Patsia <gio.patsia@gmail.com>
2023-10-23 15:17:42 +0200

    now database and backend are the same server

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -8,9 +8,21 @@ const pool = mysql.createPool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 export default pool;
+
+// const pool = mysql.createPool({
+//   MYSQLHOST: process.env.DB_HOST,
+//   MYSQLUSER: process.env.DB_USERNAME,
+//   MYSQLPASSWORD: process.env.DB_PASSWORD,
+//   MYSQLDATABASE: process.env.DB_DBNAME,
+//   MYSQLPORT: process.env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
