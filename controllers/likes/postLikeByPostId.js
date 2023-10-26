c60fc333e72ceab4de47af87bd7d93cfa11944c0
Giorgi Patsia <gio.patsia@gmail.com>
2023-10-26 11:37:28 +0200

    wip

--- controllers/likes/postLikeByPostId.js
+++ controllers/likes/postLikeByPostId.js
@@ -1,72 +1,74 @@
 import pool from "../../database/dbConnection.js";
-const { id } = id;
-const post_id = req.body.post_id;
 
-try {
-  // Delete the like record from the 'likes' table
-  await new Promise((resolve, reject) => {
-    pool.query(
-      "DELETE FROM likes WHERE post_id = ? AND user_id = ?",
-      [post_id, id],
-      (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
+const post_like_by_post_id = async (req, res) => {
+  const { id } = id.decoded.user_id;
+  const post_id = req.body.post_id;
+
+  try {
+    // Delete the like record from the 'likes' table
+    await new Promise((resolve, reject) => {
+      pool.query(
+        "DELETE FROM likes WHERE post_id = ? AND user_id = ?",
+        [post_id, id],
+        (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
         }
-      }
-    );
-  });
+      );
+    });
 
-  // Retrieve the post_author_id from the 'posts' table
-  const postAuthorId = await new Promise((resolve, reject) => {
-    pool.query(
-      "SELECT user_id FROM posts WHERE post_id = ?",
-      [post_id],
-      (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results[0].user_id);
+    // Retrieve the post_author_id from the 'posts' table
+    const postAuthorId = await new Promise((resolve, reject) => {
+      pool.query(
+        "SELECT user_id FROM posts WHERE post_id = ?",
+        [post_id],
+        (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results[0].user_id);
+          }
         }
-      }
-    );
-  });
+      );
+    });
 
-  // Delete the corresponding notification from the 'notifications' table
-  await new Promise((resolve, reject) => {
-    pool.query(
-      "DELETE FROM notifications WHERE sender_id = ? AND receiver_id = ? AND type = ?",
-      [id, postAuthorId, "Liked your post"],
-      (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
+    // Delete the corresponding notification from the 'notifications' table
+    await new Promise((resolve, reject) => {
+      pool.query(
+        "DELETE FROM notifications WHERE sender_id = ? AND receiver_id = ? AND type = ?",
+        [id, postAuthorId, "Liked your post"],
+        (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
         }
-      }
-    );
-  });
+      );
+    });
 
-  // Update the 'likes' column in the 'posts' table
-  await new Promise((resolve, reject) => {
-    pool.query(
-      "UPDATE posts SET likes = likes - 1 WHERE post_id = ?",
-      [post_id],
-      (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
+    // Update the 'likes' column in the 'posts' table
+    await new Promise((resolve, reject) => {
+      pool.query(
+        "UPDATE posts SET likes = likes - 1 WHERE post_id = ?",
+        [post_id],
+        (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
         }
-      }
-    );
-  });
+      );
+    });
 
-  res.status(200).json({ message: "Post unliked." });
-} catch (error) {
-  console.error(error);
-  res.status(500).json({ message: "Internal Server Error" });
-}
-
+    res.status(200).json({ message: "Post unliked." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 export default post_like_by_post_id;
