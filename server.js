d06a0d886778c855410a08e737454ee62ec3a69c
Giorgi Patsia <gio.patsia@gmail.com>
2023-12-06 16:01:43 +0100

    wip

--- server.js
+++ server.js
@@ -43,11 +43,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: [
-    "https://lifebook-frontend.vercel.app",
-    "http://localhost:5173",
-    "https://jsonlink.io/",
-  ],
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   Credentials: true,
 };
