baab47afc1a8a19ff1c76292b7a113bada1931c8
Giorgi Patsia <gio.patsia@gmail.com>
2023-12-06 16:49:13 +0100

    wip

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -4,11 +4,11 @@ import mysql from "mysql2";
 
 // create the connection to databasee
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
